aaefe781e01f2988f91a9ac5145f45c61c247eef
RENZOZ78 <didierbap@gmail.com>
2022-09-27 18:49:49 +0200

    v3

--- js/theme.js
+++ js/theme.js
@@ -1,11 +1,32 @@
 // js Document
 
-    // Created on   : 07/06/2018.
-    // Theme Name   : Conis.
-    // Description  : Conis - Business And Finance HTML Template
-    // Version      : 1.0.
-    // Author       : @CreativeGigs.
-    // Developed by : Jubayer al hasan. (jubayer.hasan1991@gmail.com)
+ src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+   
+    $(document).ready(function(){
+      // Add smooth scrolling to all links
+      $("a").on('click', function(event) {
+    
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+    
+          // Store hash
+          var hash = this.hash;
+    
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 500, function(){
+    
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+      });
+    });
+   
 
 
 (function($) {
@@ -125,7 +146,7 @@
             overlayer: true,
             loader: 'none',
             minHeight: '650px',
-            time: 400000,
+            time: 4000,
           });
         };
 
@@ -145,7 +166,7 @@
             overlayer: true,
             loader: 'none',
             minHeight: '650px',
-            time: 400000,
+            time: 4000,
           });
         };
 
